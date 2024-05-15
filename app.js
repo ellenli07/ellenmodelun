dc8ddd1ce0f10a3e4bfb93535d898b5a31ac8a30
ellenli07 <168687241+ellenli07@users.noreply.github.com>
2024-05-15 15:11:08 +1200

    15/05 Navigation bar with comments

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+//sets a variable for hamMenu
 const hamMenu = document.querySelector('.ham-menu');
 
+//sets a variable for the offScreenMenu
 const offScreenMenu = document.querySelector('.off-screen-menu');
 
+//when the user clicks the hamburger menu, the active class is toggled for both the hamMenu and the offScreenMenu
 hamMenu.addEventListener('click',() =>{
 	hamMenu.classList.toggle('active');
 	offScreenMenu.classList.toggle('active');
