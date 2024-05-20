3d7ae1d8595223a6327246f7831cb52cf8ca9eec
ellenli07 <168687241+ellenli07@users.noreply.github.com>
2024-05-20 19:49:09 +1200

    20/05 Implementing text slide animation on Our Story page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ['./build/*.htm'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        //This sets the keyframes of the slide animation.
+        slidein: {
+          from: {
+            //This means that the element starts invisible and slightly above its end position
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+          to: {
+            //This means that the element ends opaque and at its end position
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+      },
+      //This sets how long the animation takes, and sets a slidein-delay variable. When the animation ends, the text is shown again with forwards, meaning that the element will retain the style values that is set on the last keyframe.
+      animation: {
+        slidein: "slidein 1s ease var(--slidein-delay, 0) forwards",
+      },
+    },
   },
   plugins: [],
 }
